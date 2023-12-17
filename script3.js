b33365a4519484f5e911c7372cd2bcf2bec5a59c
Tim9079 <123922152+Tim9079@users.noreply.github.com>
2023-12-17 19:24:09 +0600

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,15 @@
+function sayHello(name) {
+    document.write("Привет, " + name + "!");
+}
+
+document.write(sayHello("Danyl"));
+
+function add(a, b) {
+    return a * b;
+}
+
+const result = add(2, 2);
+
+document.write("2 во второй степени:" + result);
+
+
